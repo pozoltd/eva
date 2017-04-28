c9aa2d837104051f2bfc0e1209280594d2c6d69d
Darth Vader <pozoltd@gmail.com>
2017-04-28 23:43:16 +1200

    Fix stuff

---
+++ metadata.php
@@ -0,0 +1,47 @@
+<?php
+ 
+$CMS_METAS = array(
+    'id',
+    'slug',
+    'modelId',
+    'rank',
+    'parentId',
+    'added',
+    'modified',
+    'active',
+);
+
+$CMS_WIDGETS = array(
+    '\\Pz\\Forms\\Types\\AssetPicker' => 'Asset picker',
+    '\\Pz\\Forms\\Types\\AssetFolderPicker' => 'Asset folder picker',
+    '\\Pz\\Forms\\Types\\Blocks' => 'Blocks',
+    'choice' => 'Choice',
+    '\\Pz\\Forms\\Types\\ChoiceMultiJson' => 'Choice Multi JSON',
+    'checkbox' => 'Checkbox',
+    '\\Pz\\Forms\\Types\\DatePicker' => 'Date picker',
+    '\\Pz\\Forms\\Types\\DateTimePicker' => 'Date time picker',
+    '\\Pz\\Forms\\Types\\FormBuilder' => 'Form builder',
+    '\\Pz\\Forms\\Types\\FormData' => 'Form data',
+    'email' => 'Email',
+    'hidden' => 'Hidden',
+    'password' => 'Password',
+    'text' => 'Text',
+    'textarea' => 'Textarea',
+    '\\Pz\\Forms\\Types\\Wysiwyg' => 'Wysiwyg',
+);
+
+
+$FORM_WIDGETS = array(
+    'choice' => 'Choice',
+    'checkbox' => 'Checkbox',
+    '\\Pz\\Forms\\Types\\DatePicker' => 'Date picker',
+    '\\Pz\\Forms\\Types\\DateTimePicker' => 'Date time picker',
+    'email' => 'Email',
+    'hidden' => 'Hidden',
+    'text' => 'Text',
+    'textarea' => 'Textarea',
+    'repeated' => 'Repeated',
+    '\\Pz\\Forms\\Types\\Wysiwyg' => 'Wysiwyg',
+    'submit' => 'Submit',
+);
+?>
---
+++ src/Eva/Application.php
@@ -0,0 +1,12 @@
+<?php
+namespace Eva;
+
+class Application extends \Silex\Application
+{
+
+    use \Silex\Application\UrlGeneratorTrait;
+    use \Silex\Application\SecurityTrait;
+    use \Silex\Application\TwigTrait;
+    use \Silex\Application\FormTrait;
+
+}
